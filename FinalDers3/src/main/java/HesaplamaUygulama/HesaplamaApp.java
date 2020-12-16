1fb9627f21f41b41126a76f0cdef5fed375ba843
oguzgunaydin <oguzhan.gunaydin@std.medipol.edu.tr>
2020-12-16 21:14:33 +0300

    Hesaplama Islemleri

--- FinalDers3/src/main/java/HesaplamaUygulama/HesaplamaApp.java
+++ FinalDers3/src/main/java/HesaplamaUygulama/HesaplamaApp.java
@@ -13,4 +13,10 @@ public class HesaplamaApp {
 	public static int toplama(int sayi1, int sayi2) {
 		return sayi1 + sayi2;
 	}
+	public static int carpma(int sayi1, int sayi2) {
+		return sayi1 * sayi2;
+	}
+	public static int bolme(int sayi1, int sayi2) {
+		return sayi1 / sayi2;
+	}
 }
